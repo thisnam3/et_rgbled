6f1aa6ebb987b481d06682571e8b31165f417890
Daniel <danierik15airsa@gmail.com>
2021-10-07 12:52:01 +0200

    led teszt

--- rgb_led/rgb_led.ino
+++ rgb_led/rgb_led.ino
@@ -1,8 +1,26 @@
 int r=9;
 int b=10;
 int g=11;
+int d=500;
 void setup() {
-
+pinMode (r, OUTPUT);
+pinMode (b, OUTPUT);
+pinMode (g, OUTPUT);
+digitalWrite(r,1);
+delay(d);
+digitalWrite(r,0);
+digitalWrite(b,1);
+delay(d);
+digitalWrite(b,0);
+digitalWrite(g,1);
+delay(d);
+digitalWrite(r,1);
+digitalWrite(b,1);
+digitalWrite(g,1);
+delay(d);
+digitalWrite(r,0);
+digitalWrite(b,0);
+digitalWrite(g,0);
 }
 
 void loop() {
